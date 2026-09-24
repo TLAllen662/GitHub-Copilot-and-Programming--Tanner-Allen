02bff86974c0ce230a69ecef7691cdb9ea2b7482
Tanner Allen <tlallen1419@gmail.com>
2026-09-24 17:23:12 +0000

    Update app.run configuration to specify port for deployment

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         seed_data()
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
